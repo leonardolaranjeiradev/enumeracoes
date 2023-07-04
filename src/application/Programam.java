3ff5d283b4498d413114423a6f72e9e922d4e3ea
Leonardo Laranjeira <leonardolaranjeiradev@gmail.com>
2023-07-03 21:00:14 -0300

    Iniciando enumeracoes

---
+++ src/application/Programam.java
@@ -0,0 +1,26 @@
+package application;
+import java.util.Date;
+
+import entities.Order;
+import entities.enums.OrderStatus;
+
+public class Programam {
+
+	public static void main(String[] args) {
+		
+		Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+		
+		System.out.println(order);
+		
+	
+		
+		
+		
+
+		
+		
+		
+
+	}
+
+}
